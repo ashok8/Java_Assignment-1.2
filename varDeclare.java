0cac3c0f0341fc9adcd5c9f984ae597dcd0dac72
ashok8 <ashok8@users.noreply.github.com>
2016-11-04 18:54:10 +0530

    Add files via upload

---
+++ varDeclare.java
@@ -0,0 +1,15 @@
+public class varDeclare {
+
+	public static void main(String[] args) {
+		int a =20, b =10;
+		b= a-- - --a;
+		System.out.println(a);
+		System.out.println(b);
+		int c=a--;
+		System.out.println(c);
+		int d=a>>2;
+		System.out.println(d);
+		int e=a&b;
+		System.out.println(e);
+		}
+}
